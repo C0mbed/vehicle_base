e1a394bdcc09ec0885d591faffa5bf6047c6afe6
C0mbed <matt@farncombe.co>
2020-08-10 12:44:10 -0600

    Integrate bootstrap, finish initial landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,25 @@
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-    configure do
-        set :public_folder, 'public'
-        set :views, 'app/views'
-        enable :sessions
-        set :session_secret, "vehicle_base" 
-    end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "vehicle_base"
+    set layout: true
+  end
 
-    get '/' do
-        erb :home
-    end
+  get '/' do
+    erb :login
+  end
 
-    post '/login' do
-        @user = User.find(email: params[:user_name], password: params[:password])
-        session[:user_id] = @user.id
-        binding.pry
-    end
+  post '/login' do
+    @user = User.find(email: params[:user_name], password: params[:password])
+    session[:user_id] = @user.id
+    binding.pry
+  end
 
-    post '/sign_up' do
-        binding.pry
-    end
+  post '/sign_up' do
+    binding.pry
+  end
 end
