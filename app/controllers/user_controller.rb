a4074a07193a6c950eb97c5de9a8c51fb02ba3a6
C0mbed <matt@farncombe.co>
2020-08-18 19:38:20 -0600

    fix issue with sessions and login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,12 +5,9 @@ class UserController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    #set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
     set layout: true
-    use Rack::Session::Cookie, :key => 'rack.session',
-        :path => '/',
-        :expire_after => 1200, # In seconds
-        :secret => "6dd501044121ee37f33d691bc79c672c0c4d0c34ad3ceebac5d1b658542f621cfba2f01a718e6164441705d74c934b29b4e2fa9b8e9b3278df38534182b15e21"
   end
 
   get '/' do
@@ -24,7 +21,8 @@ class UserController < Sinatra::Base
   post '/login' do
     @user = User.find_by(email: params[:login])
     if @user&.authenticate(params[:password])
-      session[:user_id] = @user.id
+      @session = session
+      @session[:user_id] = @user.id
       redirect '/vehicles'
     else
       erb :'user/authorize'
--- app/controllers/vehicle_controller.rb
+++ app/controllers/vehicle_controller.rb
@@ -5,12 +5,9 @@ class VehicleController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
     set layout: true
-    use Rack::Session::Cookie, :key => 'rack.session',
-        :path => '/',
-        :expire_after => 1200, # In seconds
-        :secret => "6dd501044121ee37f33d691bc79c672c0c4d0c34ad3ceebac5d1b658542f621cfba2f01a718e6164441705d74c934b29b4e2fa9b8e9b3278df38534182b15e21"
   end
 
   get '/vehicles' do
@@ -34,6 +31,7 @@ class VehicleController < Sinatra::Base
     @vehicle = Vehicle.create(params)
     @vehicle.user_id = session[:user_id]
     @vehicle.save
+
     redirect '/vehicles'
   end
 
@@ -65,7 +63,7 @@ class VehicleController < Sinatra::Base
 
   delete '/vehicles/:id/delete' do
     @vehicle = Vehicle.find(params[:id])
-    if current_user.id == @vehicle.id
+    if current_user.id == @vehicle.user_id
       @vehicle.destroy
 
       redirect '/vehicles'
